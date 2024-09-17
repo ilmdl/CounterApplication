77dc0bc9824c4a8faf82983592cd37b07fae29e2
ILmdl <ilmdl@example.com>
2024-09-17 08:50:34 +0300

    Highscore calculator implemented

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 ## Counter application
 This application should increase, decrease or reset a counter when triggered.
 This is accomplished by running functions in JavaScript that alter the html code
+
+With a basic counter working here are some improvements
+#### Version 1
+Add a high score
+
+#### Version 2
+Add a history
+
+#### Version 2.5
+Only add unique values to history
--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const increasebtn = document.getElementById("bt1");
 const decreasebtn = document.getElementById("bt2");
 const resetbtn = document.getElementById("bt3");
 
+const counter = document.getElementById("counter");
+const highscore = document.getElementById("Highscore");
+
 let value = 0;
+let array = [];
 
 increasebtn.onclick = function() {
     value++;
@@ -14,6 +18,18 @@ decreasebtn.onclick = function() {
 }
 
 resetbtn.onclick = function() {
+    array.push(value)
     value = 0;
+    highscore.textContent = maxval(array);
     counter.textContent = value;
+}
+
+function maxval(listofnumber) {
+    let bignumber = 0
+    for(let i = 0; i < listofnumber.length; i++) {
+        if (listofnumber[i] > bignumber) {
+            bignumber = listofnumber[i];
+        }
+    }
+    return bignumber;
 }
